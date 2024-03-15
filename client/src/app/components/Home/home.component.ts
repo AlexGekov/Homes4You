fb7290c2b2208445dcd099c7448c281062f16034
AlexGekov <soter69420@gmail.com>
2024-03-15 15:42:30 +0200

    Big UPDATE 0.0
    Fixed overall css + routing

--- client/src/app/components/Home/home.component.ts
+++ client/src/app/components/Home/home.component.ts
@@ -3,8 +3,8 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-Home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home-design.css']
+    templateUrl: '/home.component.html',
+    styleUrls: ['/home.component.css']
 })
 
 export class HomeComponent {}
